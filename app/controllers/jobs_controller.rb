9d895a3b6c032a5637b17ed9cdb8b91fee507727
escray <zescray@gmail.com>
2017-01-10 05:37:54 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  before_action :authenticate_admin!,
+  before_action :authenticate_user!,
           only: [:new, :create, :update, :edit, :destroy]
 
   def index
