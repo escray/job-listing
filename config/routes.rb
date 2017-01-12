8e8b3b7bcbdd7189850109067154d56e88876361
escray <zescray@gmail.com>
2017-01-12 19:25:47 +0800

    fix some bugs in admin index.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
-  resources :jobs do
-    member do
-      post :publish
-      post :hide
-    end
-  end
+  resources :jobs
+  
   namespace :admin do
-    resources :jobs
+    resources :jobs do
+      member do
+        post :publish
+        post :hide
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'jobs#index'
