62ce3d86298301fa1c7be287d40b76718bf11cbb
escray <zescray@gmail.com>
2017-01-12 19:10:37 +0800

    publish & hide

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
-  resources :jobs
+  resources :jobs do
+    member do
+      post :publish
+      post :hide
+    end
+  end
   namespace :admin do
     resources :jobs
   end
