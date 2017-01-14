43d4971d8d7a252b5029806a4a5a145094a3ee10
escray <zescray@gmail.com>
2017-01-14 15:03:51 +0800

    add resume model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170110220547) do
+ActiveRecord::Schema.define(version: 20170114022251) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20170110220547) do
     t.integer  "wage_lower_bound", default: 0
     t.string   "contact_email",    default: "xdite@growth.school"
     t.boolean  "is_hidden",        default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.integer  "job_id"
+    t.integer  "user_id"
+    t.text     "context"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
